6ff435d7ab1be9720a307ebc9395340de9b6bd21
Boyan Boyanov <88735692+boyan-boyanov@users.noreply.github.com>
2023-07-02 18:13:04 +0300

    Add json to test app

--- REST_API/index.js
+++ REST_API/index.js
@@ -3,4 +3,9 @@ const express = require('express');
 
 const app = express();
 
+//тест дали работи
+app.get('/', (req, res) => {
+    res.json({message: 'Hello'});
+})
+
 app.listen(3000, () => console.log('App listening on port 3000'));
